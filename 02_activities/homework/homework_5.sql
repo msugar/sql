2e5f93c2968bd88824d964a8178bc10906ba3009
msugar <2496555+msugar@users.noreply.github.com>
2024-09-15 18:50:38 +0000

    Add more comments

--- 02_activities/homework/homework_5.sql
+++ 02_activities/homework/homework_5.sql
@@ -9,17 +9,23 @@ Think a bit about the row counts: how many distinct vendors, product names are t
 How many customers are there (y). 
 Before your final group by you should have the product of those two queries (x*y).  */
 
-WITH _price_list AS (
+
+WITH
+	-- Retrieve the highest price for each product offered by each vendor
+	_price_list AS (
 	SELECT 
 		vi.vendor_id,
 		v.vendor_name,
 		vi.product_id, 
-		max(vi.original_price) AS price -- use the highest price in case there are more than one
+		-- Use the highest price in case there are more than one:
+		max(vi.original_price) AS price
 	FROM vendor_inventory vi
 	INNER JOIN vendor v
 		ON vi.vendor_id = v.vendor_id
 	GROUP BY vi.vendor_id, v.vendor_name, vi.product_id
 )
+-- Cross join each product with all customers, and compute total sale per each
+-- combination assuming each customer buys 5 units
 SELECT
 	pl.vendor_name, 
 	p_x_c.product_name,
@@ -40,9 +46,12 @@ This table will contain only products where the `product_qty_type = 'unit'`.
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
 
--- To make it easier to re-run this SQL script
+-- Optionally delete the table if it already exists, to make it easier to
+-- re-run this SQL script:
 -- DROP TABLE IF EXISTS product_units;
 
+-- Create a new table product_units containing products sold by unit, and add a
+-- snapshot timestamp
 CREATE TABLE product_units AS
 	SELECT *, CURRENT_TIMESTAMP AS snapshot_timestamp
 	FROM product
@@ -51,6 +60,7 @@ CREATE TABLE product_units AS
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
 
+-- Duplicate the Apple Pie product row with an updated timestamp
 INSERT INTO product_units
 SELECT 
 	product_id,
@@ -68,6 +78,7 @@ LIMIT 1;
 
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
 
+-- Delete the oldest entry of the Apple Pie product based on the timestamp
 DELETE FROM product_units 
 WHERE (product_id, snapshot_timestamp) = (
 	SELECT x.product_id, min(x.snapshot_timestamp) -- Oldest
@@ -80,6 +91,8 @@ WHERE (product_id, snapshot_timestamp) = (
 /* 1.We want to add the current_quantity to the product_units table. 
 First, add a new column, current_quantity to the table using the following syntax.*/
 
+-- Add a current_quantity column to store the latest quantity from vendor
+-- inventory
 ALTER TABLE product_units
 ADD current_quantity INT;
 
@@ -93,7 +106,9 @@ Finally, make sure you have a WHERE statement to update the right row,
 	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
 When you have all of these components, you can run the update statement. */
 
-WITH 
+WITH
+	-- Retrieve the latest quantity per product, using the most recent
+	-- market date
 	_last_quantity_per_product AS (
 		SELECT * FROM (
 			-- Using a 'sum' and 'group by' is not strictly necessary for the data we have,
@@ -108,12 +123,16 @@ WITH
 		) 
 		WHERE rn = 1
 	),
+	-- Ensure all products from product table are included, assigning 0 if
+	-- no vendor inventory is available
 	_quantity_update AS (
 		SELECT p.product_id, coalesce(quantity_per_product, 0.0) as quantity
 		FROM _last_quantity_per_product lqp
 		RIGHT JOIN product p
 			ON lqp.product_id = p.product_id
 	)
+-- Update the current_quantity in product_units with the latest quantity from
+-- vendor inventory (or zero, if it's not in any vendor inventory)
 UPDATE product_units
 SET 
 	current_quantity = s.quantity
@@ -122,4 +141,3 @@ FROM
 	_quantity_update s
 WHERE
 	product_units.product_id = s.product_id;
-	
